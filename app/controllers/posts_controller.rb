f56e867db76dd0249d1509bae30272df74cc29ae
Mohd Rizvan <mohd.rizvan@navyuginfo.com>
2018-03-11 11:30:26 +0530

    create post action for post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,7 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
+      params["post"]["user_id"] = current_user.id
       params.require(:post).permit(:title, :url, :user_id)
     end
 end
