31571cbbc2c296242ab9efe4716a874e4523f5a1
Mohd Rizvan <mohd.rizvan@navyuginfo.com>
2018-03-11 18:17:38 +0530

    test fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,12 +16,10 @@
 
 # require 'rspec/rails'
 # note: require 'devise' after require 'rspec/rails'
-require 'devise'
+# require 'devise'
 
 RSpec.configure do |config|
-
-  config.include Devise::TestHelpers, :type => :controller
-
+  
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
